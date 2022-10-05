c142b9f8e543b77e059fa8c74bed938b3de91ab3
Jabberwocky238 <7176656@qq.com>
2022-10-05 20:53:04 +0800

    none

--- index-js.js
+++ index-js.js
@@ -4,6 +4,13 @@ $(document).ready(function(){
     $(".drawer").click(()=>{
         $("nav").toggle("fast")
     });
+    $.getJSON("./context.json",function(res,sta,xhr){
+        const jsonobj = JSON.parse(res);
+        const articles = jsonobj.articles;
+        articles.forEach(element => {
+            $("nav").append("<a>"+ element.title +"</a>");
+        });
+    })
     $("article").load("contexts/1.txt")
 
     // var xhttp = new XMLHttpRequest();
@@ -14,7 +21,4 @@ $(document).ready(function(){
     // xhttp.open("GET","./contexts/1.txt",true);
     // xhttp.send();
 
-    var file = new FileReader();
-    file.readAsText("./contexts/1.txt");
-    alert(file.result);
 })
