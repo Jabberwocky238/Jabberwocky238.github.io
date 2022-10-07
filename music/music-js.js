e504cfddf352363203f72e10d1280897385a3699
Jabberwocky238 <7176656@qq.com>
2022-10-07 17:28:13 +0800

    2022/10/7 4

--- music/music-js.js
+++ music/music-js.js
@@ -37,4 +37,41 @@ $(document).ready(function(){
                 })
             });
         })
+
+        $.ajax({
+            url:"music.json",
+            type:'get',
+            cache:false,
+            async:true,
+            dataType:"json",
+            timeout:"30000",
+            contentType:"application/json;X-Content-Type-Options:nosniff",
+            success: (json)=>{
+                this.set
+                var jsonobj = json;
+                var musicinfo = jsonobj.musicinfo;
+                musicinfo.forEach(element => {
+                    const song = element.song;
+                    const detail = element.detail;
+    
+                    const pic = "album-pic/";
+                    const mus = "musics/";
+    
+                    var navchild = "<div id='" + song + "'>"+ song +"</div>";
+                    $("nav").append(navchild);
+    
+                    $("#"+song).click(()=>{
+                        
+                        $(".info").text(song);
+    
+                        $(".main img").attr("src",pic + song + ".jpg");
+                        $(".main .detail").text(detail);
+                        $(".main audio source").attr("src",mus + song + ".mp3");
+                        $(".main audio source").attr("type","audio/mpeg");
+                        // src="musics/Let You Down.mp3" type="audio/mpeg"
+                        // $(".main a").attr()
+                    })
+                });
+            }
+        })
 })
