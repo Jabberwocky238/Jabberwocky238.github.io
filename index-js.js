d6e6f2cf96bbedaa6b3199265029209c240688ba
Jabberwocky238 <7176656@qq.com>
2022-11-05 15:59:31 +0800

    2022/11/5

--- index-js.js
+++ index-js.js
@@ -9,34 +9,6 @@ $(document).ready(function(){
         $(".more-list").toggle("fast")
     });
     //AJAX part
-
-    //获取名言名句
-    
-    // $.getJSON("./context.json",function(json){
-    //     const jsonobj = json;
-    //     const articles = jsonobj.articles;
-    //     articles.forEach(element => {
-    //         //加载正文标题
-    //         $("nav").append("<div id='context" + element.number + "'>"+ element.title +"</div>");
-    //         //加载正文内容
-    //         $("#context" + element.number).click(()=>{
-    //             $(".title").text(element.title)
-
-    //             if(Array.isArray(element.context)){
-    //                 var str = "";
-    //                 element.context.forEach(passage =>{
-    //                     str = str + passage + "<br>"
-    //                 })
-    //                 $(".context").html(str)
-    //             }
-    //             else{
-    //                 $(".context").text(element.context)
-    //             }
-                
-    //         })
-    //     });
-    // })
-
     $.ajax({
         url:"./context.json",
         type:'get',
