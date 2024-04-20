b52e3f5dd4565f4045be7d5d69b23ae4a246a9e6
jabberwocky238 <jabberwocky238@gmail.com>
2024-04-20 21:00:55 +0800

    asdasdasd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Jabberwocky238.github.io',
+  // base: 'main',
   integrations: [react()]
 } // output: "server",
 );
