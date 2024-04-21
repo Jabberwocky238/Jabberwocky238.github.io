16652c0d461dc44f3f363746d60c7c2c31d43b7e
jabberwocky238 <jabberwocky238@gmail.com>
2024-04-21 11:18:07 +0800

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'), // 将 @ 指向 src 目录
+      'P': resolve(__dirname, 'public') // 将 P 指向 src/pages 目录
+    }
+  }
 })
