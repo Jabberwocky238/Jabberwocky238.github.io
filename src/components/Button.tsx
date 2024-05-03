8452096283fb852502ba16c6c9bc4643e983a11c
jabberwocky238 <jabberwocky238@gmail.com>
2024-05-03 23:57:00 +0800

    不报错了

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,22 @@
+import { CSSProperties } from 'react';
 import './Button.css'
 
 export interface ButtonProps {
     color?: string; // 按钮颜色
     onClick?: () => void;
     navigateTo?: string,
-    children: string;
+    children: string | React.ReactNode;
 }
 
 export default function Button(props: ButtonProps) {
     const { color, children, onClick, navigateTo } = props;
     console.log(children)
-    const style = {
+    const _style = {
         '--btn-color': color || 'red'
-    }
+    } as CSSProperties
     return (
         <div className='button-container'>
-            <div className='button-link' style={style}>
+            <div className='button-link' style={_style}>
                 {navigateTo
                 ? <a className='a' href={navigateTo}>{children}</a>
                 : <div className='a' onClick={onClick}>{children}</div>}
