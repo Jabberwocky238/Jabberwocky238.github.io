42280253d5aa11b125325572de09928aeb260728
jabberwocky238 <jabberwocky238@gmail.com>
2024-05-09 23:31:00 +0800

    脚本更新 周四 2024/05/09 23:31:00.24

--- scripts/ghpage.bat
+++ scripts/ghpage.bat
@@ -4,9 +4,24 @@ chcp 65001
 @REM npm run build
 
 git add .
+if %errorlevel% neq 0 (
+    echo ERROR "git add ."
+    echo 错误代码：%errorlevel%
+    pause
+) 
 
 git commit -m "脚本更新 %date% %time%"
+if %errorlevel% neq 0 (
+    echo ERROR git commit -m "脚本更新 %date% %time%"
+    echo 错误代码：%errorlevel%
+    pause
+) 
 
 git push github main
+if %errorlevel% neq 0 (
+    echo ERROR git push github main
+    echo 错误代码：%errorlevel%
+    pause
+) 
 
 pause
