82e4439345a60e114db83ecf740b841a51ea023c
jabberwocky238 <jabberwocky238@gmail.com>
2024-05-09 23:33:26 +0800

    脚本更新 周四 2024/05/09 23:33:26.11

--- scripts/ghpage.bat
+++ scripts/ghpage.bat
@@ -2,26 +2,35 @@
 chcp 65001
 
 @REM npm run build
+@REM if %errorlevel% neq 0 (
+@REM     echo "[ERROR]" npm run build
+@REM     echo 错误代码：%errorlevel%
+@REM     pause
+@REM     @REM exit /b
+@REM ) 
 
 git add .
 if %errorlevel% neq 0 (
-    echo ERROR "git add ."
+    echo "[ERROR]" "git add ."
     echo 错误代码：%errorlevel%
     pause
+    @REM exit /b
 ) 
 
 git commit -m "脚本更新 %date% %time%"
 if %errorlevel% neq 0 (
-    echo ERROR git commit -m "脚本更新 %date% %time%"
+    echo "[ERROR]" git commit -m "脚本更新 %date% %time%"
     echo 错误代码：%errorlevel%
     pause
+    @REM exit /b
 ) 
 
 git push github main
 if %errorlevel% neq 0 (
-    echo ERROR git push github main
+    echo "[ERROR]" git push github main
     echo 错误代码：%errorlevel%
     pause
+    @REM exit /b
 ) 
 
 pause
