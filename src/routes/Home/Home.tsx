6993c964d222bf102caf7db00a3a625d72042c6f
jabberwocky238 <jabberwocky238@gmail.com>
2024-06-20 00:09:37 +0800

    2024-6-20

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -10,21 +10,11 @@ function Home() {
                 <img src="/logo.svg" className="App-logo" alt="logo" />
             </div>
             <div className='card-container'>
-                <Card title="Bird"
-                    href="/#/bird"
-                    subtitle="bird"
-                    colorUp='#f30100'
-                    colorDown='#144384' />
                 <Card title="Brain Fog"
-                    href="/#/document/english/新概念/39-Nothing to worry about.md"
+                    href="/#/document/english/看了就等于练了.md"
                     subtitle="English Learning"
                     colorUp='#f30100'
                     colorDown='#144384' />
-                <Card title="Graph"
-                    href="/#/graph"
-                    subtitle="ForceGraph demo"
-                    colorUp='#f54275'
-                    colorDown='#ffc602' />
                 {/* <NeonBoard color='purple'>
                 </NeonBoard> */}
             </div>
